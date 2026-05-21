4a63442250015856257fa23797f6725aa1061076
AnastasyaYaro <AnastasiaIaro@yandex.ru>
2026-05-21 13:25:59 +0700

    Изменения в файле main

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ async def home(request: Request, db: Session = Depends(get_db)):
         {"request": request, "rows": rows},
     )
 
-tryrtutyutyi
+efsfsgsdgegd
+
 @app.post("/submit")
 async def submit_form(
     request: Request,
